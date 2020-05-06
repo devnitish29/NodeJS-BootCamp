45f583389e88ad4f1f64c8a774ba8456332ac350
Nitish Singh <nitish.singh@rakuten.com>
2020-05-06 14:03:55 +0530

    Add basic custom logging middleware

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const morgan = require("morgan");
 // Route files
 const bootcamps = require("./routes/bootcamps");
+
+// middleware files
+const logger = require("./middleware/logger");
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
