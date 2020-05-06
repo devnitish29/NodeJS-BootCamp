b01acb0e2e6a8aca0ce78079e66ce4872c1cf545
Nitish Singh <nitish.singh@rakuten.com>
2020-05-07 00:38:53 +0530

    Add POST request

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const logger = require("./middleware/logger");
 
 const app = express();
 
+//Body Parser
+app.use(express.json());
 //Dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
