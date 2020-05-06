d5fd765cfb87b3832501d6014cba2259eb325ca5
Nitish Singh <nitish.singh@rakuten.com>
2020-05-07 00:06:45 +0530

    Add mongoose db connection

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 
 // middleware files
 const logger = require("./middleware/logger");
-//Load env vars
-dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
@@ -20,8 +25,16 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode  on port ! ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode  on port ! ${PORT}`.yellow
+      .bold
   );
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error => ${err.message}`.red);
+  //Close server & exit process
+  server.close(() => process.exit(1));
 });
