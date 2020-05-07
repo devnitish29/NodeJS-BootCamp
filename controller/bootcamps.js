69defa8a86d4ec1c3270ee6cd304f6e0a5dac72f
Nitish Singh <nitish.singh@rakuten.com>
2020-05-07 21:24:10 +0530

    Add basic error response class

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -1,5 +1,5 @@
+const ErrorResponse = require("../utils/errorResponse");
 const Bootcamp = require("../model/Bootcamp");
-
 // @desc  Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access public
@@ -25,16 +25,18 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      return res.status(400).json({
-        success: false,
-      });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
     res.status(200).json({
       success: true,
       data: bootcamp,
     });
   } catch (error) {
-    next(error);
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
     // res.status(400).json({
     //   success: false,
     // });
