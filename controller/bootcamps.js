472ccaafb7c172ba7242d35be7dc62a0086dfaee
Nitish Singh <nitish.singh@rakuten.com>
2020-05-12 20:43:17 +0530

    Add virtual and populate methods

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -28,7 +28,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   );
 
   // Finding recource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
 
   //Select Fields
   if (req.query.select) {
@@ -127,7 +127,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route PUT /api/v1/bootcamps/:id
 // @access public
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
@@ -135,6 +135,7 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
 
+  bootcamp.remove();
   res.status(200).json({ success: true, data: {} });
 });
 
