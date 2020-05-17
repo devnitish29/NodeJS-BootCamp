3d3f453c55835b59aecc85a1a1abc88ab9d3641b
Nitish Singh <nitish.singh@rakuten.com>
2020-05-17 23:04:46 +0530

    Add role authorization middleware for select api's

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -16,7 +16,7 @@ const courseRouter = require("./courses");
 
 const router = express.Router();
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 //Re-route into other resource routes
 router.use("/:bootcampId/courses", courseRouter);
@@ -26,11 +26,11 @@ router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 router
   .route("/")
   .get(advancedResults(Bootcamp, "courses"), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize("publisher", "admin"), createBootcamp);
 
 router
   .route("/:id")
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp)
+  .put(protect, authorize("publisher", "admin"), updateBootcamp)
+  .delete(protect, authorize("publisher", "admin"), deleteBootcamp)
   .get(getBootcamp);
 module.exports = router;
