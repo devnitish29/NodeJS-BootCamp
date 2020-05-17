12feec3d0ec6b57a9c0f09ce36a9d544dc25e7d5
Nitish Singh <nitish.singh@rakuten.com>
2020-05-17 22:07:57 +0530

    Create Login Api

--- controller/auth.js
+++ controller/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require("../middleware/async");
 const User = require("../model/User");
 
 // @desc  Register user
-// @route GET /api/v1/auth/register
+// @route POST /api/v1/auth/register
 // @access public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -14,6 +14,37 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+// @desc  Login user
+// @route POST /api/v1/auth/login
+// @access public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //validate email & password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse("Please provide an email and password !!!", 400)
+    );
+  }
+
+  //Check for user
+  const user = await User.findOne({ email: email }).select("+password");
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials !!!", 401));
+  }
+
+  //Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials !!!", 401));
+  }
 
   const token = user.getSignedJwtToken();
 
