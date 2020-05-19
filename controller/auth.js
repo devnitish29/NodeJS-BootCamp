fac806af9288acf3bd955c4269407f3938fce782
Nitish Singh <nitish.singh@rakuten.com>
2020-05-19 21:20:55 +0530

    Add user detail update Api 	Add user password update Api

--- controller/auth.js
+++ controller/auth.js
@@ -58,6 +58,39 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: user });
 });
 
+// @desc  Update user details
+// @route PUT /api/v1/auth/updatedetails
+// @access private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fiedlsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const user = await User.findByIdAndUpdate(req.user.id, fiedlsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
+
+// @desc  Update user password
+// @route PUT /api/v1/auth/updatepassword
+// @access private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  //Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Password incorrect !!!", 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // @desc  Forgot Password
 // @route POST /api/v1/auth/forgotpassword
 // @access public
