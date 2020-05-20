313d13860db5cc8527996787f11ec0589b8a81cb
Nitish Singh <nitish.singh@rakuten.com>
2020-05-20 21:29:52 +0530

    Add update review api 	Add delete review api

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { getReviews, getReview, addReview } = require("../controller/reviews");
+const {
+  getReviews,
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview,
+} = require("../controller/reviews");
 const Review = require("../model/Review");
 const advancedResults = require("../middleware/advancedResults");
 
@@ -18,5 +24,9 @@ router
   )
   .post(protect, authorize("user", "admin"), addReview);
 
-router.route("/:id").get(getReview);
+router
+  .route("/:id")
+  .get(getReview)
+  .put(protect, authorize("user", "admin"), updateReview)
+  .delete(protect, authorize("user", "admin"), deleteReview);
 module.exports = router;
